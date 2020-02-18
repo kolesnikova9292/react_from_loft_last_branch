62ed2199d23ce30be792fe728b4e2df6b011376d
Yulia Davidenko <kolesnikova@quorum.local>
2020-02-18 19:27:43 +0300

    start adding material ui

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,8 +1,59 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import { Logo } from "loft-taxi-mui-theme";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "#ffffff",
+  },
+}));
 
 export const Header = props => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+          <Logo edge="start" color="inherit" aria-label="menu"></Logo>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <Button
+            color="inherit"
+            onClick={() => props.showMapEvent("my-map")}
+            id="my-map"
+          >
+            Карта
+          </Button>
+          <Button
+            color="inherit"
+            onClick={() => props.showMapEvent("personal-area")}
+            id="personal-area"
+          >
+            Профиль
+          </Button>
+          <Button
+            color="inherit"
+            onClick={() => props.showMapEvent("logout")}
+            id="logout"
+          >
+            Войти
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+    /*<div>
       <div onClick={() => props.showMapEvent("my-map")} id="my-map">
         Карта
       </div>
@@ -15,6 +66,6 @@ export const Header = props => {
       <div onClick={() => props.showMapEvent("logout")} id="logout">
         Выйти
       </div>
-    </div>
+    </div>*/
   );
 };
