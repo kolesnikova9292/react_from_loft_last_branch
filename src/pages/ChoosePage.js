11a84e02a320ddfb5e9e8f1d6c200f7c0ff53b43
Yulia Davidenko <kolesnikova@quorum.local>
2020-03-02 17:03:01 +0300

    adding switch in route

--- src/pages/ChoosePage.js
+++ src/pages/ChoosePage.js
@@ -3,23 +3,26 @@ import { Map } from "./map/Map";
 import { PersonalArea } from "./personal_area/PersonalArea";
 import { Login } from "./login";
 import { Registration } from "./registration";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { AuthContext } from "../providers/AuthContext";
 
 export const ChoosePage = () => {
   const loginFromContext = React.useContext(AuthContext);
   return (
     <>
-      <Route path="/map" component={Map} exact />
-      <Route path="/personal" component={PersonalArea} exact />
-      <Route path="/registration" component={Registration} exact />
-      <Route path="/login" component={Login} exact />
-      <Route path="/logout" component={Login} exact />
-      <Route
-        path="/"
-        component={loginFromContext.isAuthorized == true ? Map : Login}
-        exact
-      />
+      <Switch>
+        <Route path="/map" component={Map} exact />
+        <Route path="/personal" component={PersonalArea} exact />
+        <Route path="/registration" component={Registration} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/logout" component={Login} exact />
+        <Route
+          path="/"
+          component={loginFromContext.isAuthorized == true ? Map : Login}
+          exact
+        />
+        <Redirect to="/" />
+      </Switch>
     </>
   );
 };
