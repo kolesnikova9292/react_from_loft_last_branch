f510a8098b5cfd66d4cebb564bf3343c3a740209
Yulia Davidenko <kolesnikova@quorum.local>
2020-03-17 16:08:30 +0300

    added a mask in validation but in future should add a requirement

--- src/pages/personal_area/PersonalAreaForm.js
+++ src/pages/personal_area/PersonalAreaForm.js
@@ -3,7 +3,28 @@ import TextField from "@material-ui/core/TextField";
 import { connect } from "react-redux";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
+import MaskedInput from "react-text-mask";
 import "./PersonalArea.css";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import PropTypes from "prop-types";
+
+function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={ref => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={[/[0-1]/, /\d/, "/", /\d/, /\d/]}
+      placeholderChar={"\u2000"}
+      showMask
+    />
+  );
+}
 
 const PersonalAreaForm = () => {
   const { cardNumber, setCardNumber } = useState("");
@@ -19,6 +40,18 @@ const PersonalAreaForm = () => {
   const handleChangeCardOwner = event => {};
 
   const handleChangeCardCVC = event => {};
+
+  const [values, setValues] = useState({
+    textmask: "  /  ",
+    numberformat: "1320",
+  });
+
+  const handleChange = name => event => {
+    setValues({
+      ...values,
+      [name]: event.target.value,
+    });
+  };
 
   return (
     <div className="commonDiv">
@@ -36,16 +69,22 @@ const PersonalAreaForm = () => {
                 className="textField"
                 required
               />
-              <TextField
-                label="Срок действия"
-                color="secondary"
-                type="text"
-                name="validity"
-                value={validity}
-                onChange={handleChangeCardValidity}
-                className="textField"
-                required
-              />
+              <div className="classForMask">
+                <FormControl required>
+                  <InputLabel htmlFor="formatted-text-mask-input">
+                    Срок действия
+                  </InputLabel>
+                  <Input
+                    required
+                    name="validity"
+                    className="inpotForMask"
+                    value={validity}
+                    onChange={handleChangeCardValidity}
+                    id="formatted-text-mask-input"
+                    inputComponent={TextMaskCustom}
+                  />
+                </FormControl>
+              </div>
             </Card>
           </div>
           <div className="divForFormPersonalChild">
