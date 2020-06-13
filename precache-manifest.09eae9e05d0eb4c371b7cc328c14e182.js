9fac5396e82e619fa0826a1058663cdd8fdd1339
kolesnikova9292 <kolesnikova9292@mail.ru>
2020-06-13 15:40:50 +0300

    Updates

---
+++ precache-manifest.09eae9e05d0eb4c371b7cc328c14e182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853d7658937df9ff70a01af35298e310",
+    "url": "/loft-taxi/index.html"
+  },
+  {
+    "revision": "d09480e58cb10ccb7a59",
+    "url": "/loft-taxi/static/css/main.86db0f40.chunk.css"
+  },
+  {
+    "revision": "3bdd594bd38f83e438d1",
+    "url": "/loft-taxi/static/js/2.ab0a7102.chunk.js"
+  },
+  {
+    "revision": "ef34ccf1f9508e08d719f4d849a82231",
+    "url": "/loft-taxi/static/js/2.ab0a7102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09480e58cb10ccb7a59",
+    "url": "/loft-taxi/static/js/main.d10ca446.chunk.js"
+  },
+  {
+    "revision": "abcb575e924f501adfe9",
+    "url": "/loft-taxi/static/js/runtime-main.af8752ef.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/loft-taxi/static/media/logo.ee7cd8ed.svg"
+  }
+]);
